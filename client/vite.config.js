06b58831bdb2815b030fa73428d241feff3b57ff
danielve25 <danielvelascocuentaestudiantil@gmail.com>
2025-12-30 23:32:47 -0500

    añadir tailwind css y poner optimizacion de chuncks de librerias y imagenes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import imagemin from '@vheemstra/vite-plugin-imagemin';
+import gifsicle from 'imagemin-gifsicle';
+import jpegtran from 'imagemin-jpegtran';
+import optipng from 'imagemin-optipng';
+import svgo from 'imagemin-svgo';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    imagemin({
+      plugins: {
+        jpg: jpegtran({ progressive: true }),
+        png: optipng({ optimizationLevel: 5 }),
+        gif: gifsicle({ interlaced: true }),
+        svg: svgo({
+          plugins: [
+            {
+              name: 'removeViewBox',
+              active: false,
+            },
+          ],
+        }),
+      },
+      // Optional: create webp versions
+      makeWebp: {
+        plugins: {
+          // Configuration for imagemin-webp
+        },
+      },
+      // Optional: include/exclude filters, etc.
+      include: /\\.(png|jpg|jpeg|gif|svg)$/i,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router'],
+          axios: ['axios'],
+        },
+      },
+    },
+  },
+});
